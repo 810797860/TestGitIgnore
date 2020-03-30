9ee147f81231565691283dd817277f11e6b42f9a
810797860 <810797860@qq.com>
2020-03-30 16:59:32 +0800

    测试是不是会有target的jar包

---
+++ src/main/java/com/otoko/TestGitIgnoreApplication.java
@@ -0,0 +1,13 @@
+package com.otoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitIgnoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitIgnoreApplication.class, args);
+	}
+
+}
