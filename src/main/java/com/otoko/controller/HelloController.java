d9c0c82d460d15ac83ef63b3522e838b3f333a8a
810797860 <810797860@qq.com>
2020-03-31 11:00:36 +0800

    测试jib(3)(完成一期，先存个档)

--- src/main/java/com/otoko/controller/HelloController.java
+++ src/main/java/com/otoko/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.otoko.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2020/3/31.
  */
@@ -12,6 +14,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String toHello(){
-        return "hello";
+        return new Date().toString();
+    }
+
+    @GetMapping("/hello2")
+    public String toHello2(){
+        return new Date().toString();
     }
 }
